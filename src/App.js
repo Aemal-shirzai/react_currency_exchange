e0e067b1630b007b33f87557668fe96c4d2a6177
aemal-shirzai <aemalshirzai2016@gmail.com>
2023-03-16 01:25:32 -0400

    [IMP] Add history.

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ function App() {
   const [usdAmount, setUsdAmount] = useState(0)
   const [euroAmount, setEuroAmount] = useState(0)
   const [isEuro, setIsEuro] = useState(true)
-  
+  const [overrideFxRate, setOverrideFxRate] = useState('');
+  const [history, setHistory] = useState([]);
+
 
   useEffect(() => {
     const updateFxRate = () => setFxRate(prev => prev + randomNumbers(0.5, -0.5))
@@ -20,16 +22,23 @@ function App() {
 
   useEffect(() => {
     exchangeAmount()
-  }, [isEuro])
+  }, [isEuro, overrideFxRate, fxRate])
+
+  useEffect(() => {
+    if (overrideFxRate && (Math.abs(overrideFxRate - fxRate) / fxRate) * 100 > 200) {
+      setOverrideFxRate('')
+    }
+  }, [fxRate, overrideFxRate])
 
   const exchangeAmount = () => {
+    const exchangeRate = overrideFxRate || fxRate
     console.log(".>>> euro amount", mainInputRef.current.value)
-    console.log(">>>>>> fx rate", fxRate, isEuro);
+    console.log(">>>>>> fx rate", exchangeRate, isEuro);
     if (isEuro) {
-      setUsdAmount(() => mainInputRef.current.value * fxRate)
+      setUsdAmount(() => mainInputRef.current.value * exchangeRate)
       setEuroAmount(mainInputRef.current.value)
     } else {
-      setEuroAmount(() => mainInputRef.current.value / fxRate)
+      setEuroAmount(() => mainInputRef.current.value / exchangeRate)
       setUsdAmount(mainInputRef.current.value)
     }
   }
@@ -38,35 +47,98 @@ function App() {
     setIsEuro(prev => !prev)
   }
 
+  const handleOverride = (e) => {
+    setOverrideFxRate(e.target.value)
+  }
+
+  const handleFormSumbit = (e) => {
+    e.preventDefault();
+    setHistory(prevData => [{
+      fxRate: overrideFxRate || fxRate,
+      overrideFxRate: overrideFxRate !== '',
+      amount: isEuro ? `${euroAmount} €` : `${usdAmount} $`,
+      convertedAmount: isEuro ? `${usdAmount} $` : `${euroAmount} €`
+    }, ...prevData.slice(0, 4)]);
+  }
+
 
 
   return (
     <div className="container">
-      <h3>Current FX Rate: {fxRate}</h3>
 
-      <div>
-        <form>
-          <div className="row">
-            <div className="col-sm-5">
-              <div className="input-group mb-3">
-                <span className="input-group-text">{isEuro ? '€' : '$'}</span>
-                <span className="input-group-text">{isEuro ? 'Euro' : 'USD'}</span>
-                <input type="number" className="form-control" ref={mainInputRef} value={isEuro ? euroAmount : usdAmount} onChange={exchangeAmount}/>
-              </div>
-            </div>
-            <div className="d-flex col-sm-2 justify-content-center align-items-center" id="exchange_icon" onClick={handleSwitch}>
-              <img src={exchangeIcon} className="img-fluid"/>
-            </div>
-            <div className="col-sm-5">
-              <div className="input-group mb-3">
-                <span className="input-group-text">{!isEuro ? '€' : '$'}</span>
-                <span className="input-group-text">{!isEuro ? 'Euro' : 'USD'}</span>
-                <input type="number" className="form-control" value={!isEuro ? euroAmount : usdAmount} disabled/>
-              </div>
+      {/* Rate Part */}
+      <div className="row">
+        <div className="col-sm-6">
+          <div className="input-group mb-3">
+            <span className="input-group-text">Realtime EURO/USD FX-Rate</span>
+            <input type="number" className="form-control" value={fxRate} disabled />
+          </div>
+        </div>
+        <div className="col-sm-6">
+          <div className="input-group mb-3">
+            <span className="input-group-text">Override EURO/USD FX-Rate</span>
+            <input type="number" className="form-control" value={overrideFxRate} onChange={handleOverride} />
+          </div>
+        </div>
+      </div>
+
+
+      {/* Exchange form */}
+
+      <form onSubmit={handleFormSumbit}>
+        <div className="row">
+          <div className="col-sm-5">
+            <div className="input-group mb-3">
+              <span className="input-group-text">{isEuro ? '€' : '$'}</span>
+              <span className="input-group-text">{isEuro ? 'Euro' : 'USD'}</span>
+              <input type="number" className="form-control" ref={mainInputRef} value={isEuro ? euroAmount : usdAmount} onChange={exchangeAmount} />
             </div>
           </div>
-        </form>
-      </div>
+          <div className="d-flex col-sm-2 justify-content-center align-items-center" id="exchange_icon" onClick={handleSwitch}>
+            <img src={exchangeIcon} className="img-fluid" alt="Switch" />
+          </div>
+          <div className="col-sm-5">
+            <div className="input-group mb-3">
+              <span className="input-group-text">{!isEuro ? '€' : '$'}</span>
+              <span className="input-group-text">{!isEuro ? 'Euro' : 'USD'}</span>
+              <input type="number" className="form-control" value={!isEuro ? euroAmount : usdAmount} disabled />
+            </div>
+          </div>
+        </div>
+        <button type="submit" className="btn btn-lg btn-outline-secondary" disabled={euroAmount === 0 || usdAmount === 0 }>Save to History</button>
+      </form>
+
+
+      {history.length > 0 ?
+
+        <>
+          <hr />
+          {/* History */}
+          <div className="table-responsive">
+            <table className="table table-bordered table-striped">
+              <thead>
+                <tr>
+                  <th>FX Rate</th>
+                  <th>Override</th>
+                  <th>Amount</th>
+                  <th>Converted Amount</th>
+                </tr>
+              </thead>
+              <tbody>
+                {history.map((data, index) => (
+                  <tr key={index}>
+                    <td>{data.fxRate}</td>
+                    <td>{data.overrideFxRate ? "Yes" : "No"}</td>
+                    <td>{data.amount}</td>
+                    <td>{data.convertedAmount}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </>
+
+        : null}
 
     </div>
   );
